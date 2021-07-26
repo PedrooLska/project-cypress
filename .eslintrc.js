75ba7ed8a0514e3b2b989ce4b04a71a74a7ed429
Pedro Lucas <pedro.santos@me.com.br>
2021-07-26 19:14:37 -0300

    Remove rule prettier/prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,14 +10,6 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'prettier/prettier': [
-      'warn',
-      {
-        '#': 'prettier config in here :)',
-        singleQuote: true,
-        semi: false,
-        trailingComma: 'none'
-      }
-    ]
+    'prettier/prettier': 0
   }
 }
