a37b52f9d41f16cc06ac78da4177d7e873f6da99
Pedro Lucas <pedro.santos@me.com.br>
2021-07-26 15:22:50 -0300

    Create Table Tasks

--- src/components/To-do-List.vue
+++ src/components/To-do-List.vue
@@ -15,12 +15,12 @@
             </tr>
         </thead>
         <tbody>
-            <tr v-for="task in listAllTask()" :key="task.id">
-                <th>{{ task.id }}</th>
-                <th>{{ task.name }}</th>
-                <th>
-                    <v-btn></v-btn>
-                    <v-btn></v-btn>
+            <tr v-for="task in listAllTask" :key="task.id">
+                <th class="text-left">{{ task.id }}</th>
+                <th class="text-left">{{ task.name }}</th>
+                <th class="text-right">
+                    <v-btn class="primary mr-1"><v-icon>$edit</v-icon></v-btn>
+                    <v-btn class="error"><v-icon>$edit</v-icon></v-btn>
                 </th>
             </tr>
         </tbody>
@@ -32,13 +32,13 @@
 export default {
     computed: {
         listAllTask() {
-            return $store.getters["todoList/listAllTask"]
+            return this.$store.getters["todoList/listAllTask"]
         }
     }
 }
 </script>
 
-<style lang="scss">  
+<style lang="scss">
   tbody {
     tr:hover {
         background-color: transparent !important;
